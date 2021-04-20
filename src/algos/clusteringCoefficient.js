536fe1b1b6c5707043546bfa5f5d432ae7d359e0
Jeffrey Xiao <jeffrey.xiao1@gmail.com>
2021-04-20 15:28:32 -0400

    tweak CC

--- src/algos/clusteringCoefficient.js
+++ src/algos/clusteringCoefficient.js
@@ -1,40 +1,38 @@
 // INVARIANT: only works for UNDIRECTED graph
 // calculates clustering coefficient for given node
 const clusteringCoefficient = (g, node) => {
-	const neighbors = g.adjList.get(node);
-	const numNeighbors = neighbors.length;
-	// calculates total possible nummber of pairs, i.e. n choose 2
-	const numPairs = factorial(numNeighbors) / (factorial(numNeighbors - 2) * 2);
+  const neighbors = g.adjList.get(node);
+  const numNeighbors = neighbors.length;
+  // calculates total possible nummber of pairs, i.e. n choose 2
+  const numPairs = factorial(numNeighbors) / (factorial(numNeighbors - 2) * 2);
 
-	// for each pair of nodes, check to see if there is edge between
-	let count = 0;
-	for (var i = 0; i < neighbors.length; i++) {
-		for (var j = 0; j < neighbors.length; j++) {
-			if (i !== j) {
-				const nodes = g.adjList.get(i);
-				for (var k = 0; k < nodes.length; k++) {
-					if (nodes[k] === j) {
-						count++;
-					}
-				}
-			}
-		}
-	}
+  // for each pair of nodes, check to see if there is edge between
+  let count = 0;
+  neighbors.forEach(({ node: neighborId, weight }) => {
+    neighbors.forEach(({ node: neighborId2, weight2 }) => {
+      if (neighborId !== neighborId2) {
+        const nodes = g.adjList.get(neighborId);
+        nodes.forEach(({ node: neighborId3, weight3 }) => {
+          count += neighborId3 === neighborId2 ? 1 : 0;
+        });
+      }
+    });
+  });
 
-	// divide by 2 b/c undirected
-	const numEdges = count / 2;
-	return numEdges / numPairs;
+  // divide by 2 b/c undirected
+  const numEdges = count / 2.0;
+  return numEdges / numPairs;
 };
 
 // calculates factorial
 function factorial(num) {
-	if (num === 0 || num === 1) {
-		return 1;
-	}
-	for (var i = num - 1; i > 0; i--) {
-		num *= i;
-	}
-	return num;
+  if (num === 0 || num === 1) {
+    return 1;
+  }
+  for (var i = num - 1; i > 0; i--) {
+    num *= i;
+  }
+  return num;
 }
 
 export default clusteringCoefficient;
